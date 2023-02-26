ad495f0eb4014c872744e15e29b787e184baf4c4
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-02-26 19:03:47 +0000

    Set up project

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+const Employee = require("./lib/Employee");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +11,21 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
-
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const team = [];
 
+startProgram();
+
+async function startProgram() {
+  team.push(new Employee("dan", 1, "test@test.com"));
+
+  let htmlDoc = render(team);
+
+  await fs.writeFile(outputPath, htmlDoc, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("File has been created successfully.");
+  });
+}
