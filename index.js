dd542a162c65a9504795778a204cee3dc606bce3
Daniele Trovato <plasticsoup@yahoo.co.uk>
2023-02-26 22:51:54 +0000

    Add preliminary question to get number of employees, then add for loop to create that number of employees

--- index.js
+++ index.js
@@ -17,65 +17,71 @@ const team = [];
 startProgram();
 
 async function startProgram() {
-  // const number = await inquirer.prompt([
-  //   {
-  //     type: "number",
-  //     name: "numOfEmployees",
-  //     message: "How many employees are we inputting?",
-  //   },
-  // ]);
+  //
 
-  const employeeData = [
+  const number = await inquirer.prompt([
     {
-      type: "list",
-      name: "role",
-      message: "Please choose a role below:",
-      choices: ["Manager", "Engineer", "Intern"],
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "Enter employee's entire name",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "Enter employee's email",
-    },
-  ];
-
-  const data = await inquirer.prompt(employeeData);
-
-  const role = data.role;
-  let name;
-  let message;
-  if (role === "Manager") {
-    name = "officeNumber";
-    message = "What is the manager's office number?";
-  } else if (role === "Engineer") {
-    name = "gitHub";
-    message = "What is the engineer's gitHub?";
-  } else if (role === "Intern") {
-    name = "school";
-    message = `What is the name of employee's school?`;
-  }
-
-  const roleSpecificQuestion = await inquirer.prompt([
-    {
-      type: "input",
-      name: name,
-      message: message,
+      type: "number",
+      name: "numOfEmployees",
+      message: "How many employees are we inputting?",
     },
   ]);
 
-  data[name] = name;
+  const num = number.numOfEmployees;
 
-  if (data.role === "Manager") {
-    team.push(new Manager(`${data.name}`, 1, data.email, data.officeNumber));
-  } else if (data.role === "Engineer") {
-    team.push(new Engineer(`${data.name}`, 1, data.email, data.gitHub));
-  } else if (data.role === "Intern") {
-    team.push(new Intern(`${data.name}`, 1, data.email, data.school));
+  for (let index = 0; index < num; index++) {
+    const employeeData = [
+      {
+        type: "list",
+        name: "role",
+        message: "Please choose a role below:",
+        choices: ["Manager", "Engineer", "Intern"],
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "Enter employee's entire name",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "Enter employee's email",
+      },
+    ];
+
+    const data = await inquirer.prompt(employeeData);
+
+    const role = data.role;
+    let name;
+    let message;
+    if (role === "Manager") {
+      name = "officeNumber";
+      message = "What is the manager's office number?";
+    } else if (role === "Engineer") {
+      name = "gitHub";
+      message = "What is the engineer's gitHub?";
+    } else if (role === "Intern") {
+      name = "school";
+      message = `What is the name of employee's school?`;
+    }
+
+    const roleSpecificQuestion = await inquirer.prompt([
+      {
+        type: "input",
+        name: name,
+        message: message,
+      },
+    ]);
+
+    data[name] = name;
+
+    if (data.role === "Manager") {
+      team.push(new Manager(`${data.name}`, 1, data.email, data.officeNumber));
+    } else if (data.role === "Engineer") {
+      team.push(new Engineer(`${data.name}`, 1, data.email, data.gitHub));
+    } else if (data.role === "Intern") {
+      team.push(new Intern(`${data.name}`, 1, data.email, data.school));
+    }
   }
 
   let htmlDoc = render(team);
